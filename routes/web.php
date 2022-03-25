6ba60475c3806146ee5fcd78337fccaca3e1a852
Matteo Morganti <matteo99morganti@gmail.com>
2022-03-25 17:08:05 +0100

    Push

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data = [
+        'name' => 'Matteo',
+        'lastname' => 'Morganti'
+    ];
+
+    return view('home', $data);
 });
